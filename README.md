85b3c4e3e1d4efbaef842e30fc4e7454e34c1928
vaanwd <vali29@gmail.com>
2014-02-06 15:31:37 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,70 @@
-Zeal
-====
+# Zeal for Sublime Text 2/3
 
-Zeal for Sublime Text 2/3
+Zeal package for Sublime Text using [Zeal](http://zealdocs.org/) documentation browser witch is similar with [Dash](http://kapeli.com/dash/).
+
+*Tested in Windows/Linux.*  
+
+##Screen-shots
+
+Multiple results for PHP mapping.
+
+![Multiple results for PHP mapping](http://www.vaanwebdesign.ro/includes/images/zeal_1.png)
+<br/>
+<br/>
+Custom search in Zeal docsets.
+
+![Custom search in Zeal docsets](http://www.vaanwebdesign.ro/includes/images/zeal_2.png)
+
+## Installation
+
+Easiest way to install the plugin is to use [Package Control](http://wbond.net/sublime_packages/package_control).
+
+Alternatively you can clone with git directly into `Packages` directory in the Sublime Text 2 application settings area. The directory name must be `Zeal`.
+
+### Using Git
+
+Go to your Sublime Text 2 `Packages` directory and clone the repository using the command below:
+
+    git clone https://github.com/vaanwd/Zeal "Zeal"
+
+### Download Manually
+
+* Download the files using the GitHub .zip download option
+* Unzip the files and rename the folder to `Zeal`
+* Copy the folder to your Sublime Text 2 `Packages` directory
+
+## Usage
+
+`F1` - Open Zeal documentation for current/selected word.
+
+`Shift` + `F1` - Open Zeal search bar.
+
+## Mapping example
+To mapping other language for Zeal docset you need to edit `User\Zeal.sublime-settings`:
+
+	{
+	  /**
+	   *  Zeal executable path.
+	   *  For Linux: /usr/bin/zeal
+	   *  For Windows: c:\\Program Files\\Zeal\\zeal.exe
+	   */
+	  "zeal_command": "/usr/bin/zeal",
+
+	  /**
+	   * Language mapping examples.
+	   */
+	  "language_mapping": {
+	    "HTML": {"lang": "html", "zeal_lang": "html"},
+	    "JavaScript": {"lang": "javascript", "zeal_lang": "javascript"},
+	    "CSS": {"lang": "css", "zeal_lang": "css"},
+	    "CSS MSN": {"lang": "css", "zeal_lang": "msdn"},
+	    "PHP": {"lang": "php", "zeal_lang": "php"},
+	    "Drupal": {"lang": "php", "zeal_lang": "drupal"},
+	    "Python": {"lang": "python", "zeal_lang" : "python"},
+	    "Django": {"lang": "python", "zeal_lang" : "django"}
+	  }
+	}
+
+
+[&copy; 2014 Vaan Web Design](http://www.vaanwebdesign.ro)
+
---
+++ zeal.py
@@ -0,0 +1,215 @@
+import sublime
+import sublime_plugin
+import os
+import subprocess
+import shutil
+
+language = None
+
+
+def get_settings():
+    zeal_path = sublime.packages_path() + '/Zeal'
+    user_path = sublime.packages_path() + '/User'
+
+    if not os.path.isdir(user_path):
+        os.mkdir(user_path)
+
+    default_settings_path = zeal_path + '/Zeal.sublime-settings'
+    user_settings_path = user_path + '/Zeal.sublime-settings'
+    if not os.path.exists(user_settings_path):
+        shutil.copyfile(default_settings_path, user_settings_path)
+
+    return sublime.load_settings('Zeal.sublime-settings')
+
+
+def get_language(view):
+    scope = view.scope_name(view.sel()[0].begin()).strip()
+    getlang = scope.split('.')
+    language = getlang[-1]
+    if language == 'basic':
+        language = getlang[-2]
+    if language == 'html':
+        if 'php' in getlang:
+            language = 'php'
+        elif 'js' in getlang:
+            language = 'javascript'
+        elif 'css' in getlang:
+            language = 'css'
+    if language == 'js':
+        language = 'javascript'
+    if 'source.css.less' in scope:
+        language = 'less'
+    if 'source.scss' in scope:
+        language = 'scss'
+    if 'source.sass' in scope:
+        language = 'sass'
+    if 'source.actionscript.2' in scope:
+        language = 'actionscript'
+    del getlang
+    return language
+
+
+def get_css_class_or_id(view):
+    cur_pos = view.sel()[0].a
+    scope_reg = view.extract_scope(cur_pos)
+
+    def get_sym(pos):
+        sym = view.substr(sublime.Region(pos, pos + 1))
+        return sym
+
+    rule_type = set(['.', '#'])
+    delims = set([
+        ' ', '"', "'", '<', '>', '(', ')', '/', '\n', ':',
+    ])
+    all_delims = rule_type | delims
+    left = cur_pos
+    while get_sym(left) in delims:
+        left -= 1
+    while left > scope_reg.a and get_sym(left) not in all_delims:
+        left -= 1
+    if get_sym(left) in all_delims:
+        left += 1
+    right = cur_pos
+    while right < scope_reg.b and get_sym(right) not in all_delims:
+        right += 1
+    return view.substr(sublime.Region(left, right))
+
+
+def selection(view):
+
+    def IsNotNull(value):
+        return value is not None and len(value) > 1
+
+    def badChars(sel):
+        bad_characters = [
+            '/', '\\', ':', '\n', '{', '}', '(', ')',
+            '<', '>', '[', ']', '|', '?', '*', ' ',
+            '""', "'",
+        ]
+        for letter in bad_characters:
+            sel = sel.replace(letter, '')
+        return sel
+
+    selection = ''
+    for region in view.sel():
+        selection += badChars(view.substr(region))
+    if IsNotNull(selection):
+        return selection
+    else:
+        curr_sel = view.sel()[0]
+        word = view.word(curr_sel)
+        selection = badChars(view.substr(word))
+        if IsNotNull(selection):
+            return selection
+        else:
+            return None
+    return None
+
+
+def get_word(view):
+    word = None
+    if language == 'css' or language == 'scss' or language == 'sass' \
+        or language == 'less':
+        word = get_css_class_or_id(view)
+    else:
+        word = selection(view)
+    return word
+
+
+def open_zeal(lang, text, join_command):
+    zeal_exe = get_settings().get('zeal_command')
+
+    if os.path.isfile(zeal_exe):
+        try:
+            cmd = []
+            cmd.append(zeal_exe)
+            cmd.append(u"--query")
+            cmd.append(u"'")
+            if join_command:
+                cmd.append(text)
+            else:
+                cmd.append(lang)
+                cmd.append(u":")
+                cmd.append(text)
+            cmd.append(u"'")
+            subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=False)
+        except Exception as e:
+            sublime.status_message("Zeal - (%s)" % (e))
+    else:
+        sublime.error_message('Could not find your %s executable.\n\nPlease edit Zeal.sublime-settings' % (zeal_exe))
+
+
+class ZealSearchSelectionCommand(sublime_plugin.TextCommand):
+
+    def no_word_selected(self):
+        sublime.status_message('No word was selected.')
+
+    def run(self, edit, **kwargs):
+        global language
+        language = get_language(self.view.window().active_view())
+        text = ""
+
+        for selection in self.view.sel():
+            if selection.empty():
+                text = self.view.word(selection)
+
+            text = self.view.substr(selection)
+
+            if text == "":
+                text = get_word(self.view)
+
+            if text is None:
+                self.no_word_selected()
+            else:
+                language_mapping = get_settings().get('language_mapping')
+                items = dict()
+                popup_list = []
+
+                for item in language_mapping.items():
+                    if item[1]['lang'] == language:
+                        items[item[0]] = item[1]
+
+                if len(items) > 1:
+                    for title, files in items.items():
+                        popup_list.append([title, 'Language: %s' % (files['lang'])])
+                elif len(items) == 1:
+                    open_zeal(language, text, False)
+                else:
+                    sublime.status_message('No Zeal mapping was found for %s language.' % (language))
+
+            def callback(idx):
+                if idx == -1:
+                    return
+                self.selected_item = popup_list[idx]
+                open_zeal(items[self.selected_item[0]]['zeal_lang'], text, False)
+
+            if text:
+                if len(kwargs) == 0:
+                    self.view.window().show_quick_panel(popup_list, callback, sublime.MONOSPACE_FONT)
+                elif len(kwargs) != 0:
+                    self.selected_item = kwargs['title']
+                    open_zeal(items[self.selected_item]['zeal_lang'], text, False)
+
+
+class ZealSearchCommand(sublime_plugin.TextCommand):
+
+    last_text = ''
+
+    def run(self, edit):
+        view = self.view
+        self.view_panel = view.window().show_input_panel('Search in Zeal for:', self.last_text, self.after_input, self.on_change, None)
+        self.view_panel.set_name('zeal_command_bar')
+
+    def after_input(self, text):
+        if text.strip() == "":
+            self.last_text = ''
+            sublime.status_message("No text was entered")
+            return
+        else:
+            open_zeal("", text, True)
+
+    def on_change(self, text):
+        if text.strip() == "":
+            return
+
+        self.last_text = text.strip()
